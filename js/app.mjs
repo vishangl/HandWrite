1496d654adf75dc20ea2538ed1f50a776e00b0d3
vishangl <161571588+vishangl@users.noreply.github.com>
2026-02-02 21:12:35 +0530

    added some more chnages

--- js/app.mjs
+++ js/app.mjs
@@ -181,26 +181,29 @@ document.querySelectorAll('.switch-toggle input').forEach((toggleInput) => {
  * Set GitHub Contributors
  */
 
-fetch(
-  'https://api.github.com/repos/saurabhdaware/text-to-handwriting/contributors'
-)
-  .then((res) => res.json())
-  .then((res) => {
-    document.querySelector('#project-contributors').innerHTML = res
-      .map(
-        (contributor) => /* html */ `
-        <div class="contributor-profile shadow">
-          <a href="${contributor.html_url}">
-            <img 
-              alt="GitHub avatar of contributor ${contributor.login}" 
-              class="contributor-avatar" 
-              loading="lazy" 
-              src="${contributor.avatar_url}" 
-            />
-            <div class="contributor-username">${contributor.login}</div>
-          </a>
-        </div>
-      `
-      )
-      .join('');
-  });
+/**
+ * Set GitHub Contributors - Removed
+ */
+// fetch(
+//   'https://api.github.com/repos/saurabhdaware/text-to-handwriting/contributors'
+// )
+//   .then((res) => res.json())
+//   .then((res) => {
+//     document.querySelector('#project-contributors').innerHTML = res
+//       .map(
+//         (contributor) => /* html */ `
+//         <div class="contributor-profile shadow">
+//           <a href="${contributor.html_url}">
+//             <img 
+//               alt="GitHub avatar of contributor ${contributor.login}" 
+//               class="contributor-avatar" 
+//               loading="lazy" 
+//               src="${contributor.avatar_url}" 
+//             />
+//             <div class="contributor-username">${contributor.login}</div>
+//           </a>
+//         </div>
+//       `
+//       )
+//       .join('');
+//   });
